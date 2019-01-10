e7d8d65eabcb5382916d2b23277e29878e2db616
jrash <jrash.ncsu.edu>
2019-01-09 23:47:58 -0500

    Final analysis scripts for JChem submission

---
+++ analyses/correlation_plots_figS4_S5.r
@@ -0,0 +1,111 @@
+library(psych)
+library(parcor)
+library(fpc)
+library(gplots)
+
+# You will need to replace with the path to the data directory on your machine
+setwd("G:/My Drive/FourchesLab/github/Fourches_Lab/metabochem/data/")
+train.dif.s <- read.csv("healthstate_anova_wsig_control_training_serum_nonpara.txt")
+train.dif.p <- read.csv("healthstate_anova_wsig_control_training_plasma_nonpara.txt")
+
+load("training_set_tq_nonormalize.rda")
+d$Set <- paste0(rep("Training", nrow(d)), d$Health_State)
+d <- d[, c(1:5, 136, 6:135)]
+d_serum <- d[d$Organ == "Serum", ]
+d_plasma <- d[d$Organ == "Plasma", ]
+health_df_serum <- d_serum
+health_df_plasma <- d_plasma
+
+test.dif.s <- read.csv("healthstate_anova_wsig_control_test_serum_nonpara.txt")
+test.dif.p <- read.csv("healthstate_anova_wsig_control_test_plasma_nonpara.txt")
+
+load("test_set_tq_nonormalize.rda")
+d$Set <- paste0(rep("Test", nrow(d)), d$Health_State)
+d <- d[, c(1:5, 136, 6:135)]
+d_serum <- d[d$Organ == "Serum", ]
+d_plasma <- d[d$Organ == "Plasma", ]
+health_df_serum.test <- d_serum
+health_df_plasma.test <- d_plasma
+
+# cluster based on spearmen and partial correlations-------------
+
+single.met.list <- list(health_df_serum, health_df_serum.test,
+                        health_df_plasma, health_df_plasma.test)
+
+head(single.met.list[[4]])
+
+# regress intensity values on the covariates
+for(i in seq_along(single.met.list)) {
+  for(j in 7:ncol(single.met.list[[i]])) {
+    lm.fit <- lm(single.met.list[[i]][, j] ~ Smoking_Status + Gender, data = single.met.list[[i]])
+    single.met.list[[i]][, j] <- lm.fit$residuals
+  }
+}
+
+names(single.met.list) <- c("serum_training", "serum_test",
+                            "plasma_training", "plasma_test")
+
+dif.list <- list(train.dif.s, train.dif.s, train.dif.p, train.dif.p)
+
+hclust.average <- function(x) hclust(x, method="average")
+palette <- colorRampPalette(c("blue","yellow"))(n=300)
+
+for (i in 1:3) {
+  x <- single.met.list[[i]][, -seq(1, 6)]
+  partial.cor <- pls.net(as.matrix(x), ncomp = 30)
+  partial.cor <- partial.cor$pcor
+
+  partial.cor <- partial.cor[c(which(dif.list[[i]]$adj_pvalues < .075)),
+                             c(which(dif.list[[i]]$adj_pvalues < .075))]
+
+  colnames(partial.cor) <- colnames(single.met.list[[i]])[which(dif.list[[i]]$adj_pvalues < .075) + 6]
+  rownames(partial.cor) <- colnames(single.met.list[[i]])[which(dif.list[[i]]$adj_pvalues < .075) + 6]
+
+  d <- as.dist(1 - abs(partial.cor))
+  ks <- seq(2, nrow(as.matrix(d)) - 1, by = 1)
+  hc <- hclust(d, method = "average")
+  ASW <- sapply(ks, FUN=function(k) {
+    fpc::cluster.stats(d, cutree(hc,k))$avg.silwidth
+  })
+  nClus <- ks[which.max(ASW)]
+  tiff(paste0("../analyses/",
+             names(single.met.list)[i], "par_asw.tiff"), res = 300,
+       width = 7, height = 7, units = "in")
+  plot(y=ASW, x=ks, type="l", ylab="Average Silhouette Width", xlab="Number of Clusters",
+       xaxt = "n")
+  axis(side = 1, at = ks)
+  dev.off()
+
+  plot(hclust(d, method ="average"), main="Average Linkage with Correlation-Based Distance", xlab="", sub ="")
+
+  tiff(paste0("../analyses/",
+             names(single.met.list)[i], "par_cor.tiff"),
+       res = 300, width = 7, height = 7, units = "in")
+  heatmap.2(data.matrix(d), trace="none", density.info="none", col=palette, mar=c(10,10),cexCol=1, cexRow=1,
+            Rowv = T, Colv=T, keysize = 1,hclustfun = hclust.average)
+  dev.off()
+
+  d <- as.dist(1 - abs(cor(single.met.list[[i]][, c(which(dif.list[[i]]$adj_pvalues < .075) + 6)],
+                           method = "spearman")))
+  ks <- seq(2, nrow(as.matrix(d)) - 1, by = 1)
+  hc <- hclust(d, method = "average")
+  ASW <- sapply(ks, FUN=function(k) {
+    fpc::cluster.stats(d, cutree(hc,k))$avg.silwidth
+  })
+  nClus <- ks[which.max(ASW)]
+  tiff(paste0("../analyses/",
+             names(single.met.list)[i], "spe_asw.tiff"), res = 300,
+       width = 7, height = 7, units = "in")
+  plot(y=ASW, x=ks, type="l", ylab="Average Silhouette Width", xlab="Number of Clusters",
+       xaxt = "n")
+  axis(side = 1, at = ks)
+  dev.off()
+
+  tiff(paste0("../analyses/",
+             names(single.met.list)[i], "spe_cor.tiff"),
+       res = 300, width = 7, height = 7, units = "in")
+  heatmap.2(data.matrix(d), trace="none", density.info="none", col=palette, mar=c(10,10),cexCol=1, cexRow=1,
+            Rowv = T, Colv=T, keysize = 1,hclustfun = hclust.average)
+  dev.off()
+}
+
---
+++ analyses/metab_classifierl_functions_serum.r
@@ -0,0 +1,180 @@
+FitLasso <- function(data, data.test, all.acc) {
+  
+  cor.cols <- findCorrelation(cor(data.matrix(data)), cutoff = .9)
+  cor.cols
+  
+  x = model.matrix(Health_State ~ ., data = data)[,-1]
+  y = data$Health_State
+  grid = 10^seq(10, -2, length = 100)
+  
+  lasso.mod <- glmnet(x, y, family = "binomial", alpha = 1, lambda = grid)
+  plot(lasso.mod)
+  
+  set.seed(835)
+  cv.out = cv.glmnet(x, y, family="binomial", alpha = 1, type.measure="class", nfolds = 80,
+                     keep=TRUE)
+  bestlam = cv.out$lambda.min
+  bestlam
+  
+  lasso.prob <- cv.out$fit.preval[, cv.out$lambda == bestlam]
+  lasso.pred <- lasso.prob > .5
+  
+  # LOOCV accuracy
+  all.acc[1, 1] <- mean(lasso.pred == y)
+  
+  # specificity
+  idx <- y == 0
+  all.acc[1, 2] <- mean(lasso.pred[idx] == y[idx])
+  
+  # sensitivity
+  idx <- y == 1
+  all.acc[1, 3] <- mean(lasso.pred[idx] == y[idx])
+  
+  # AUC
+  all.acc[1, 4] <- as.numeric(auc(y, lasso.prob))
+  
+  plot(cv.out)
+  
+  cv.coef <- coef(cv.out, s = bestlam)
+  cv.coef <- as.matrix(Matrix(cv.coef, sparse = F))
+  cv.coef[cv.coef != 0, ]
+  
+  # prediction performance on test set
+  
+  #remove correlated columns
+  
+  # data.test <- data.test[, -cor.cols]
+  x.test = model.matrix(Health_State ~ ., data = data.test)[, -1]
+  y.test = data.test$Health_State
+  lasso.pred <- predict(lasso.mod, s = bestlam, newx = x.test, type = "class")
+  lasso.pred <- as.numeric(lasso.pred[, 1])
+  
+  # test accuracy
+  all.acc[1, 5] <- mean(lasso.pred == y.test)
+  
+  # specificity.test
+  idx <- y.test == 0
+  all.acc[1, 6] <- mean(y.test[idx] == lasso.pred[idx])
+  
+  # sensitivity.test
+  idx <- y.test == 1
+  all.acc[1, 7] <- mean(y.test[idx] == lasso.pred[idx])
+  
+  # AUC
+  lasso.prob <- predict(lasso.mod, s = bestlam, newx = x.test, type = "response")
+  all.acc[1, 8] <- as.numeric(auc(y.test, lasso.prob[, 1]))
+  
+  return(all.acc)
+}
+
+
+FitMlmodels <- function(data, data.test, all.acc) {
+  
+  customSummary <- function (data, lev = NULL, model = NULL){
+    spec <- specificity(data[, "pred"], data[, "obs"], lev[2])
+    pred <- factor(ifelse(data[, "neg"] > 0.5, "neg", "pos"))
+    spec2 <- specificity(pred, data[, "obs"], "pos")
+    acclist <- as.numeric()
+    for(i in seq(0.01, 0.99, 0.01)){
+      predi <- factor(ifelse(data[, "neg"] > i, "neg", "pos"), levels = c("neg", "pos"))
+      singleacc <- mean(predi == data[, "obs"])
+      acclist <- c(acclist, singleacc)
+    }
+    max(acclist) -> accmax
+    
+    out <- c(spec, spec2, accmax)
+    
+    names(out) <- c("Spec", "Spec2", "Accmax")
+    out
+  }
+  
+  fitControl <- trainControl(method = "LOOCV", classProbs = TRUE, savePredictions = TRUE,
+                             summaryFunction = customSummary)
+  models <- c("LogitBoost", 'svmRadial', 'rf', 'pls', 'xgbTree')
+  
+  for (j in 1:5) {
+    set.seed(9)
+    if (j != 2) {
+      model.fit <- train(Health_State~., method=models[j], data=data, metric = "Accmax",
+                         trControl = fitControl, tuneLength = 10)
+    } else {
+      svmGrid <- expand.grid(sigma= 2^c(-25, -20, -15,-10, -5, 0), C= 2^c(0:5))
+      model.fit <- train(Health_State~., method=models[j], data=data, metric = "Accmax",
+                         trControl = fitControl, tuneGrid = svmGrid)
+      sigma <- log(model.fit$bestTune$sigma, base = 2)
+      C <- log(model.fit$bestTune$C, base = 2)
+      set.seed(9)
+      svmGrid <- expand.grid(sigma= 2^seq(sigma - .5, sigma + .5, length.out = 10),
+                             C= 2^seq(C - 1, C + 1, length.out = 10))
+      model.fit <- train(Health_State~., method=models[j], data=data, metric = "Accmax",
+                         trControl = fitControl, tuneGrid = svmGrid)
+    }
+    
+    maxcv <- max(model.fit$results$Accmax)
+    
+    # Need to find the threshold
+    mod.pred <- as.data.frame(model.fit$pred[, 6:ncol(model.fit$pred)])
+    tune.par <- model.fit$bestTune
+    fix.order <- order(names(model.fit$bestTune))
+    tune.par <- tune.par[fix.order]
+    fix.order <- order(colnames(mod.pred))
+    mod.pred <- as.data.frame(mod.pred[, fix.order])
+    
+    sel <- vector(length = nrow(mod.pred))
+    for (i in 1:nrow(mod.pred)) {
+      sel[i] <- all(mod.pred[i, ] == tune.par)
+    }
+    
+    acclist <- as.numeric()
+    for(i in seq(0.01, 0.99, 0.001)){
+      predi <- factor(ifelse(model.fit$pred[sel, "pos"] >= i, "pos", "neg"), levels = c("pos", "neg"))
+      singleacc <- mean(predi == model.fit$pred$obs[sel])
+      acclist <- c(acclist, singleacc)
+    }
+    maxcv <- max(acclist)
+    thresh <- seq(0.01, 0.99, 0.001)[which.max(acclist)]
+    
+    y <- data$Health_State
+    
+    probs.train <- model.fit$pred[sel, "pos"]
+    pred.train <- ifelse(model.fit$pred[sel, "pos"] >= thresh, "pos", "neg")
+    
+    # LOOCV accuracy
+    all.acc[j+1, 1] <- mean(pred.train == y)
+    
+    # sensitivity
+    idx <- y == "pos"
+    all.acc[j+1, 2] <- mean(pred.train[idx] == y[idx])
+    
+    # specificity
+    idx <- y == "neg"
+    all.acc[j+1, 3] <- mean(pred.train[idx] == y[idx])
+    
+    # AUC
+    y.num <- ifelse(y == "pos", 1, 0)
+    all.acc[j+1, 4] <- as.numeric(auc(y.num, probs.train))
+    
+    # External
+    y.test <- data.test$Health_State
+    probs.test <- predict.train(model.fit, data.test, type = "prob")
+    pred.test <- ifelse(probs.test$pos > thresh, "pos", "neg")
+    
+    # test accuracy
+    all.acc[j+1, 5] <- mean(pred.test == y.test)
+    
+    # sensitivity.test
+    idx <- y.test == "pos"
+    all.acc[j+1, 6] <- mean(y.test[idx] == pred.test[idx])
+    
+    # specificity.test
+    idx <- y.test == "neg"
+    all.acc[j+1, 7] <- mean(y.test[idx] == pred.test[idx])
+    
+    # AUC
+    y.num <- ifelse(y.test == "pos", 1, 0)
+    all.acc[j+1, 8] <- as.numeric(auc(y.num, probs.test[, "pos"]))
+  } 
+  
+  return(all.acc)
+}
+
